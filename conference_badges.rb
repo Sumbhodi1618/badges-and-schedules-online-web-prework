df4e3da80ff5470f4f02fd4e36633154fab7f27f
Justin <sumbhodi1618@gmail.com>
2019-05-16 21:27:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ def assign_rooms(attendees)
   room=0
   attendees.collect do |attendee|
     room+=1
-    "Hello, #{name}! You'll be assigned to room #{room}!"
+    "Hello, #{attendee}! You'll be assigned to room #{room}!"
   end
 end
 
