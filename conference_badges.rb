7a00129f6efb4ee01549656e7fc406eb635c702b
Justin <sumbhodi1618@gmail.com>
2019-05-16 19:51:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 def badge_maker(name)
-  array.push(name)
   puts "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-      badges = []
+    badges = []
       attendees.each do |attendee|
-      badges.push("Hello, my name is #{attendee}.")
- end
+        badges.push("Hello, my name is #{attendee}.")
+      end
+ return badges
 end
