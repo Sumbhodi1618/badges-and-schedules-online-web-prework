8b6b5ac38a36dc82eeeeaa0eaf53993a30b72f1c
Justin <sumbhodi1618@gmail.com>
2019-05-16 19:45:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,10 @@ def badge_maker(name)
   array.push(name)
   puts "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+      badges = [] 
+      attendees.each do |attendee|
+      badges.push "Hello, my name is #{attendee}."
+
+end
